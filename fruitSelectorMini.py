17258c8ed240e6461658c0241e941a2cca576a6e
Leon Augustyn <leonstyn@gmail.com>
2026-07-11 14:14:32 +0200

    Update fruitSelectorMini.py
    Further improvements to interact more.
    Small changes to the runtime wording.

--- fruitSelectorMini.py
+++ fruitSelectorMini.py
@@ -1,15 +1,11 @@
-
-name = input("Would you like some fruit? ")
-
+print("Welcome to the :) Boring Fruit Selector.") 
+like_fruit = input("Hallo, what is your name? ")
+    print("Hallo,", like_fruit, "which fruit do you prefer? ")
 box_fruits = {1:"Apple", 2:"Orange",
-3:"Banana, "}
-
+3:"Banana"}
 for num in range(1, 4):
     print(num, box_fruits[num])
-    
 name = input("Which one is Your favourite? ")
-
-
 if name == "1":
     print("Nice! Healthy choice")
 elif name == "2":
